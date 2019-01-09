c945f7aafa3cfd7e0cc3bd8497666d0107bc78d9
Milind Bangar <milindbangar79@gmail.com>
2019-01-09 08:45:53 +0000

    Enhanced the custom exception class to address different scenarios from Exception handling perspective

--- src/com/resteasy/mongodb/utils/MissingEnvironmentVariableCheckedException.java
+++ src/com/resteasy/mongodb/utils/MissingEnvironmentVariableCheckedException.java
@@ -1,11 +1,28 @@
 package com.resteasy.mongodb.utils;
 
-public class MissingEnvironmentVariableCheckedException extends Exception{
+public class MissingEnvironmentVariableCheckedException extends Exception {
 
 	private static final long serialVersionUID = 1L;
-	
-	public MissingEnvironmentVariableCheckedException(String errMessage, Throwable err) {
-		super(errMessage,err);
+
+	public MissingEnvironmentVariableCheckedException() {
+		super();
+	}
+
+	public MissingEnvironmentVariableCheckedException(String errMessage, Throwable cause) {
+		super(errMessage, cause);
+	}
+
+	public MissingEnvironmentVariableCheckedException(String message, Throwable cause, boolean enableSuppression,
+			boolean writableStackTrace) {
+		super(message, cause, enableSuppression, writableStackTrace);
+	}
+
+	public MissingEnvironmentVariableCheckedException(String message) {
+		super(message);
+	}
+
+	public MissingEnvironmentVariableCheckedException(Throwable cause) {
+		super(cause);
 	}
 
 }
